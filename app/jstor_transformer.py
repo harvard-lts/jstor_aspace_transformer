ae04e151499d0f4cc2da773b86de68841faebd55
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-01-23 12:33:22 -0500

    incorporating itests, add saxon jar

--- app/jstor_transformer.py
+++ app/jstor_transformer.py
@@ -30,10 +30,9 @@ class JstorTransformer():
         return result
 
     def do_task(self, request_json):
-        """\
-Get job tracker file
-Append job ticket id to jobs in process list in the tracker file
-Update job timestamp file"""
+        """Get job tracker file
+            Append job ticket id to jobs in process list in the tracker file
+            Update job timestamp file"""
 
         result = {
           'success': False,
@@ -48,38 +47,6 @@ Update job timestamp file"""
         transformdir = "/tmp/JSTORFORUM/transformed/loebmusic"
         for filename in os.listdir(harvestdir):
             subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformdir + "/" + filename, "-s:" + harvestdir + "/" + filename, "-xsl:xslt/ssio2via.xsl"])                               
-        result['success'] = True
-        # altered line so we can see request json coming through properly
-        result['message'] = 'Job ticket id {} has completed '.format(request_json['job_ticket_id'])
-
-        #sleep_s = os.getenv("TASK_SLEEP_S", 1)
-
-        #current_app.logger.info("Sleep " + str(sleep_s) + "seconds")
-        #sleep(1)
-        
-        return result
-
-    def do_transform(filename):
-        xsltFile = open(file="xslt/ssio2via.xsl", encoding="utf-8")
-        xmlFile = open(file=filename, encoding="utf-8")
-        with PySaxonProcessor(license=False) as proc:
-            current_app.logger.info("2")
-            '''
-            xsltProc = proc.new_xslt30_processor()
-            xsltProc.set_cwd(".") 
-            xsltProc.transform_to_file(source_file=directory + "/" + filename ,stylesheet_file="xslt/ssio2via.xsl", output_file="/tmp/JSTORFORUM/transformed/loebmusic/test" + filename)
-            '''
-            xsltProc = proc.new_xslt_processor()
-            document = proc.parse_xml(xml_text=xmlFile.read())
-            xsltProc.set_source(xdm_node=document)
-            xsltProc.compile_stylesheet(stylesheet_text=xsltFile.read())
-            xsltProc.set_jit_compilation(True)
-            xsltProc.transform_to_file()
-            output = xsltProc.transform_to_string()
-            f = open("/tmp/JSTORFORUM/transformed/loebmusic/" + filename, "w")
-            f.write(output)
-            f.close()
-
         result['success'] = True
         # altered line so we can see request json coming through properly
         result['message'] = 'Job ticket id {} has completed '.format(request_json['job_ticket_id'])
