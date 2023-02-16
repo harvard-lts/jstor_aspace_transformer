92377cfda6e44e6d80dde1766d5a95c00798b3b6
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-02-16 09:57:03 -0500

    remove temp test files/dir

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -7,13 +7,39 @@ def hello_world():
 def test_hello_world():
     assert hello_world()  == 'Hello World'
 
-def test_transforms():
-    ead_ns = {'ead': 'urn:isbn:1-931666-22-9'}
+
+
+def test_geog_coordinates():
+
     if not os.path.exists("./tests/data/temp"):
-        os.makedirs("./tests/data/temp")
+        os.makedirs("./tests/data/temp")    
     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001654780_via.xml", "-s:" "./tests/data/8001654780_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
-    #with open("./tests/data/temp/8001654780_via.xml") as f:
-    #    geog_coord_xml = f.read()
     doc = ET.parse("./tests/data/temp/8001654780_via.xml")
-    recid = doc.xpath("//recordId")[0].text
-    assert recid == "8001654780"
+    #display records
+    assert doc.xpath("//surrogate/coordinates/@altitude")[0] == "2776.7"
+    assert doc.xpath("//surrogate/coordinates/latitude/@decimal")[0] == "36.6425966666667"
+    assert doc.xpath("//surrogate/coordinates/longitude/@decimal")[0] == "71.45956"
+    #TGN
+    assert doc.xpath("//location/coordinates/latitude/@degree")[0] == "36"
+    assert doc.xpath("//location/coordinates/longitude/@degree")[0] == "71"
+
+    subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001306196_via.xml", "-s:" "./tests/data/8001306196_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
+    doc2 = ET.parse("./tests/data/temp/8001306196_via.xml")
+    #topic
+    assert doc2.xpath("//topic[coordinates]/coordinates/latitude/@minute")[0] == "2"
+    assert doc2.xpath("//topic[coordinates]/coordinates/longitude/@direction")[0] == "E"
+
+    subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001694596_via.xml", "-s:" "./tests/data/8001694596_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
+    doc3 = ET.parse("./tests/data/temp/8001694596_via.xml")
+    #placeName
+    assert doc3.xpath("//placeName[coordinates]/coordinates/latitude/@decimal")[0] == "31.8167"
+    assert doc3.xpath("//placeName[coordinates]/coordinates/longitude/@degree")[0] == "8"
+
+    for i in os.listdir("./tests/data/temp"):
+        print(i)
+        os.remove("./tests/data/temp/" + i)
+    os.rmdir("./tests/data/temp")    
+    
+
+#def test_ead():
+#    ead_ns = {'ead': 'urn:isbn:1-931666-22-9'}
