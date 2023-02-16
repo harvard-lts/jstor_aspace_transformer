7b77bdf607e31ae463c13d3c5186d0962c55c583
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-02-16 10:39:47 -0500

    add placeOfProduction coordinate example

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -19,7 +19,7 @@ def test_geog_coordinates():
     assert doc.xpath("//surrogate/coordinates/@altitude")[0] == "2776.7"
     assert doc.xpath("//surrogate/coordinates/latitude/@decimal")[0] == "36.6425966666667"
     assert doc.xpath("//surrogate/coordinates/longitude/@decimal")[0] == "71.45956"
-    #TGN
+    #location
     assert doc.xpath("//location/coordinates/latitude/@degree")[0] == "36"
     assert doc.xpath("//location/coordinates/longitude/@degree")[0] == "71"
 
@@ -34,6 +34,12 @@ def test_geog_coordinates():
     #placeName
     assert doc3.xpath("//placeName[coordinates]/coordinates/latitude/@decimal")[0] == "31.8167"
     assert doc3.xpath("//placeName[coordinates]/coordinates/longitude/@degree")[0] == "8"
+
+    subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001694086_via.xml", "-s:" "./tests/data/8001694086_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
+    doc4 = ET.parse("./tests/data/temp/8001694086_via.xml")    
+    #placeOfPublication
+    assert doc4.xpath("//placeOfProduction[coordinates]/coordinates/latitude/@decimal")[0] == "21.0"
+    assert doc4.xpath("//placeOfProduction[coordinates]/coordinates/longitude/@degree")[0] == "84"
 
     for i in os.listdir("./tests/data/temp"):
         print(i)
