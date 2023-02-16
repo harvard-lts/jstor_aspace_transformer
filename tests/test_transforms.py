a921e715cdd1af80092459396aaefb7f92a7ab98
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-02-16 10:44:21 -0500

    not ssio id for each test case

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -15,29 +15,29 @@ def test_geog_coordinates():
         os.makedirs("./tests/data/temp")    
     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001654780_via.xml", "-s:" "./tests/data/8001654780_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
     doc = ET.parse("./tests/data/temp/8001654780_via.xml")
-    #display records
+    #display records (8001654780)
     assert doc.xpath("//surrogate/coordinates/@altitude")[0] == "2776.7"
     assert doc.xpath("//surrogate/coordinates/latitude/@decimal")[0] == "36.6425966666667"
     assert doc.xpath("//surrogate/coordinates/longitude/@decimal")[0] == "71.45956"
-    #location
+    #location (8001654780)
     assert doc.xpath("//location/coordinates/latitude/@degree")[0] == "36"
     assert doc.xpath("//location/coordinates/longitude/@degree")[0] == "71"
 
     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001306196_via.xml", "-s:" "./tests/data/8001306196_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
     doc2 = ET.parse("./tests/data/temp/8001306196_via.xml")
-    #topic
+    #topic (8001306196)
     assert doc2.xpath("//topic[coordinates]/coordinates/latitude/@minute")[0] == "2"
     assert doc2.xpath("//topic[coordinates]/coordinates/longitude/@direction")[0] == "E"
 
     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001694596_via.xml", "-s:" "./tests/data/8001694596_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
     doc3 = ET.parse("./tests/data/temp/8001694596_via.xml")
-    #placeName
+    #placeName (8001694596)
     assert doc3.xpath("//placeName[coordinates]/coordinates/latitude/@decimal")[0] == "31.8167"
     assert doc3.xpath("//placeName[coordinates]/coordinates/longitude/@degree")[0] == "8"
 
     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" "./tests/data/temp/8001694086_via.xml", "-s:" "./tests/data/8001694086_ssio.xml", "-xsl:xslt/ssio2via.xsl"])  
     doc4 = ET.parse("./tests/data/temp/8001694086_via.xml")    
-    #placeOfPublication
+    #placeOfPublication (8001694086)
     assert doc4.xpath("//placeOfProduction[coordinates]/coordinates/latitude/@decimal")[0] == "21.0"
     assert doc4.xpath("//placeOfProduction[coordinates]/coordinates/longitude/@degree")[0] == "84"
 
