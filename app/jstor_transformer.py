f6d895499f2a010ebad4951df1421fdc0d8eb73a
Chip Goines <chip_goines@harvard.edu>
2023-02-27 01:18:21 -0500

    first pass

--- app/jstor_transformer.py
+++ app/jstor_transformer.py
@@ -16,6 +16,7 @@ class JstorTransformer():
         self.parent_job_ticket_id = None
         self.child_error_count = 0
         self.max_child_errors = int(os.getenv("CHILD_ERROR_LIMIT", 10))
+        self.repositories = self.load_repositories()
 
     # Write to error log update result and update job tracker file
     def handle_errors(self, result, error_msg, exception_msg = None, set_job_failed = False):
@@ -48,6 +49,12 @@ class JstorTransformer():
 
         #dump json
         current_app.logger.info("json message: " + json.dumps(request_json))
+        job_ticket_id = str(request_json['job_ticket_id'])
+
+        #integration test: write small record to mongo to prove connectivity
+        integration_test = False
+        if ('integration_test' in request_json):
+            integration_test = request_json['integration_test']
 
         harvestset = None
         if 'harvestset' in request_json:
@@ -58,24 +65,29 @@ class JstorTransformer():
             current_app.logger.info("running jstorforum transform")
             jstorforum = request_json['jstorforum']
         if jstorforum:
-            self.do_transform('jstorforum', harvestset)
+            try:
+                if (integration_test):
+                    self.do_transform('jstorforum', None, job_ticket_id, True)
+                else:
+                    self.do_transform('jstorforum', harvestset, job_ticket_id)
+            except:
+                current_app.logger.error("Error: unable to transform jstorforum records, {}", err)
 
         aspace = False
         if 'aspace' in request_json:
             current_app.logger.info("running aspace transform")
             aspace = request_json['aspace']
         if aspace:
-            self.do_transform('aspace', None)
+            try:
+                if (integration_test):
+                    self.do_transform('aspace', None, job_ticket_id, True)
+                else:
+                    self.do_transform('aspace', None, job_ticket_id)
+            except Exception as err:
+                current_app.logger.error("Error: unable to transform aspace records, {}", err)
 
-        #integration test: write small record to mongo to prove connectivity
-        integration_test = False
-        if ('integration_test' in request_json):
-            integration_test = request_json['integration_test']
         if (integration_test):
             current_app.logger.info("running integration test")
-            #do a transform using the test config file
-            self.do_transform('jstorforum', None, True)
-            self.do_transform('aspace', None, True)
             try:
                 mongo_url = os.environ.get('MONGO_URL')
                 mongo_dbname = os.environ.get('MONGO_DBNAME')
@@ -84,7 +96,6 @@ class JstorTransformer():
 
                 mongo_db = mongo_client[mongo_dbname]
                 integration_collection = mongo_db[mongo_collection]
-                job_ticket_id = str(request_json['job_ticket_id'])
                 test_id = "transformer-" + job_ticket_id
                 test_record = { "id": test_id, "status": "inserted" }
                 integration_collection.insert_one(test_record)
@@ -98,7 +109,7 @@ class JstorTransformer():
 
         return result
 
-    def do_transform(self, jobname, harvestset, itest=False):
+    def do_transform(self, jobname, harvestset, job_ticket_id, itest=False):
         if itest:
             configfile = "harvestjobs_test.json"
         else:
@@ -108,14 +119,32 @@ class JstorTransformer():
             harvjobsjson = f.read()
         harvestconfig = json.loads(harvjobsjson)
         #current_app.logger.debug("harvestconfig")        
-        #current_app.logger.debug(harvestconfig) 
+        #current_app.logger.debug(harvestconfig)
+        mongo_url = os.environ.get('MONGO_URL')
+        mongo_dbname = os.environ.get('MONGO_DBNAME')
+        harvest_collection_name = os.environ.get('HARVEST_COLLECTION', 'jstor_harvests')
+        repository_collection_name = os.environ.get('REPOSITORY_COLLECTION', 'jstor_repositories')
+        record_collection_name = os.environ.get('RECORD_COLLECTION', 'jstor_records')
+        mongo_url = os.environ.get('MONGO_URL')
+        mongo_client = None
+        mongo_db = None
+        try:
+            mongo_client = MongoClient(mongo_url, maxPoolSize=1)
+            mongo_db = mongo_client[mongo_dbname]
+        except Exception as err:
+            current_app.logger.error("Error: unable to connect to mongodb, {}", err)
+
         harvestDir = os.getenv("jstor_harvest_dir") + "/"         
         transformDir = os.getenv("jstor_transform_dir") + "/" 
         for job in harvestconfig:     
             if jobname == 'jstorforum' and jobname == job["jobName"]:   
                 for set in job["harvests"]["sets"]:
                     setSpec = "{}".format(set["setSpec"])
+                    repository_name = self.repositories[setSpec]
                     opDir = set["opDir"]
+                    totalHarvestCount = 0
+                    harvestdate = datetime.today().strftime('%Y-%m-%d') 
+
                     if harvestset is None:
                         if os.path.exists(harvestDir + opDir + "_oaiwrapped"):
                             if len(fnmatch.filter(os.listdir(harvestDir + opDir + "_oaiwrapped"), '*.xml')) > 0:
@@ -125,6 +154,16 @@ class JstorTransformer():
                                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + opDir + "/" + filename, "-s:" + harvestDir + opDir + "/" + filename, "-xsl:xslt/ssio2via.xsl"])                               
                                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + opDir + "_hollis/" + filename, "-s:" + transformDir + opDir + "/" + filename, "-xsl:xslt/via2hollis.xsl"])                               
                                     current_app.logger.info("DONE transforming: " + filename)
+                                    #write/update record
+                                    try:
+                                        status = "update"
+                                        identifier = filename[:4]
+                                        self.write_record(job_ticket_id, identifier, harvestdate, setSpec, repository_name, 
+                                           status, record_collection_name, mongo_db)
+                                        totalHarvestCount = totalHarvestCount + 1    
+                                    except Exception as e:
+                                        current_app.logger.error(e)
+                                        current_app.logger.error("Mongo error writing " + setSpec + " record: " +  identifier)
                     elif  setSpec == harvestset:
                         if os.path.exists(harvestDir + opDir + "_oaiwrapped"):
                             if len(fnmatch.filter(os.listdir(harvestDir + opDir + "_oaiwrapped"), '*.xml')) > 0:
@@ -134,8 +173,26 @@ class JstorTransformer():
                                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + opDir + "/" + filename, "-s:" + harvestDir + opDir + "/" + filename, "-xsl:xslt/ssio2via.xsl"])                               
                                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + opDir + "_hollis/" + filename, "-s:" + transformDir + opDir + "/" + filename, "-xsl:xslt/via2hollis.xsl"])                               
                                     current_app.logger.info("DONE transforming: " + filename)
+                                    #write/update record
+                                    try:
+                                        status = "update"
+                                        identifier = filename[:4]
+                                        self.write_record(job_ticket_id, identifier, harvestdate, setSpec, repository_name, 
+                                           status, record_collection_name, mongo_db)
+                                        totalHarvestCount = totalHarvestCount + 1    
+                                    except Exception as e:
+                                        current_app.logger.error(e)
+                                        current_app.logger.error("Mongo error writing " + setSpec + " record: " +  identifier)
+                    #update harvest record
+                    try:
+                        self.update_harvest(job_ticket_id, harvestdate, setSpec, 
+                            repository_name, totalHarvestCount, harvest_collection_name, mongo_db)
+                    except Exception as e:
+                        current_app.logger.error(e)
+                        current_app.logger.error("Mongo error writing harvest record for : " +  setSpec)
 
-            if jobname == 'aspace' and jobname == job["jobName"]:    
+            if jobname == 'aspace' and jobname == job["jobName"]:
+                harvestdate = datetime.today().strftime('%Y-%m-%d')     
                 for filename in os.listdir(harvestDir + 'aspace'):
                     current_app.logger.info("begin transforming: " + filename)
                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + "aspace_stripwrapper/" + filename, "-s:" + harvestDir + "aspace/" + filename, "-xsl:xslt/strip_oai_aspace.xsl"])                               
@@ -144,6 +201,108 @@ class JstorTransformer():
                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + "aspace_hollis_part1/" + filename, "-s:" + transformDir + "aspace_harvard/" + filename, "-xsl:xslt/ead2hollis_part1.xsl"])  
                     subprocess.call(["java", "-jar", "lib/saxon9he-xslt-2-support.jar", "-o:" + transformDir + "aspace_hollis/" + filename, "-s:" + transformDir + "aspace_hollis_part1/" + filename, "-xsl:xslt/ead2hollis_part2.xsl"])  
                     current_app.logger.info("DONE transforming: " + filename)
+                    #write/update record
+                    try:
+                        status = "update"
+                        identifier = filename[:4]
+                        self.write_record(job_ticket_id, identifier, harvestdate, "0000", "aspace", 
+                            status, record_collection_name, mongo_db)
+                        totalHarvestCount = totalHarvestCount + 1    
+                    except Exception as e:
+                        current_app.logger.error(e)
+                        current_app.logger.error("Mongo error writing aspace record: " +  identifier)
+
+                #update harvest record
+                try:
+                    self.update_harvest(job_ticket_id, harvestdate, "0000", 
+                        "aspace", totalHarvestCount, harvest_collection_name, mongo_db)
+                except Exception as e:
+                    current_app.logger.error(e)
+                    current_app.logger.error("Mongo error writing harvest record for: aspace")
+
+    def update_harvest(self, harvest_id, harvest_date, repository_id, repository_name,
+            total_harvested, collection_name, mongo_db):
+        if mongo_db == None:
+            current_app.logger.info("Error: mongo db not instantiated")
+            return
+        try:
+            if harvest_date == None: #set harvest date to today if harvest date is None
+                harvest_date = datetime.today().strftime('%Y-%m-%d') 
+            harvest_date_obj = datetime.strptime(harvest_date, "%Y-%m-%d")
+            query = { "id" : harvest_id }
+            harvest_record = {"$set": { "id": harvest_id, "harvest_date": harvest_date_obj, 
+                "repository_id": repository_id, "repository_name": repository_name, 
+                "total_transformed_count": total_harvested, "success": False } }
+            harvest_collection = mongo_db[collection_name]
+            #harvest_collection.insert_one(harvest_record)
+            harvest_collection.update_one(query, harvest_record, upsert=True)
+            current_app.logger.info(repository_name + " harvest for " + harvest_date + " written to mongo ")
+        except Exception as err:
+            current_app.logger.info("Error: unable to connect to mongodb, {}", err)
+        return
+
+    def write_record(self, harvest_id, record_id, harvest_date, repository_id, repository_name,
+            status, collection_name, mongo_db):
+        if mongo_db == None:
+            current_app.logger.info("Error: mongo db not instantiated")
+            return
+        try:
+            if harvest_date == None: #set harvest date to today if harvest date is None
+                harvest_date = datetime.today().strftime('%Y-%m-%d')  
+            harvest_date_obj = datetime.strptime(harvest_date, "%Y-%m-%d")
+            harvest_record = {"$set":  { "id": harvest_id, "last_update": harvest_date_obj, "record_id": record_id, 
+                "repository_id": repository_id, "repository_name": repository_name, 
+                "status": status, "transformed": True } }
+            record_collection = mongo_db[collection_name]
+            #record_collection.insert_one(harvest_record)
+            record_collection.update_one(query, harvest_record, upsert=True)
+            current_app.logger.info("record " + str(record_id) + " of repo " + str(repository_id) + " written to mongo ")
+        except Exception as err:
+            current_app.logger.info("Error: unable to connect to mongodb, {}", err)
+        return
+
+    def write_record(self, harvest_id, record_id, harvest_date, repository_id, repository_name,
+            status, collection_name, mongo_db):
+        if mongo_db == None:
+            current_app.logger.info("Error: mongo db not instantiated")
+            return
+        try:
+            if harvest_date == None: #set harvest date to today if harvest date is None
+                harvest_date = datetime.today().strftime('%Y-%m-%d')  
+            harvest_date_obj = datetime.strptime(harvest_date, "%Y-%m-%d")
+            harvest_record = {"$set":  { "id": harvest_id, "last_update": harvest_date_obj, "record_id": record_id, 
+                "repository_id": repository_id, "repository_name": repository_name, 
+                "status": status, "published": True } }
+            record_collection = mongo_db[collection_name]
+            #record_collection.insert_one(harvest_record)
+            record_collection.update_one(query, harvest_record, upsert=True)
+            current_app.logger.info("record " + str(record_id) + " of repo " + str(repository_id) + " written to mongo ")
+        except Exception as err:
+            current_app.logger.info("Error: unable to connect to mongodb, {}", err)
+        return
+
+    def load_repositories(self):
+        repositories = {}
+        try:
+            mongo_url = os.environ.get('MONGO_URL')
+            mongo_dbname = os.environ.get('MONGO_DBNAME')
+            repository_collection_name = os.environ.get('REPOSITORY_COLLECTION', 'jstor_repositories')
+            mongo_url = os.environ.get('MONGO_URL')
+            mongo_dbname = os.environ.get('MONGO_DBNAME')
+            mongo_client = MongoClient(mongo_url, maxPoolSize=1)
+
+            mongo_db = mongo_client[mongo_dbname]
+            repository_collection = mongo_db[repository_collection_name]
+            repos = repository_collection.find({})
+            for r in repos:
+                k = r["_id"]
+                v = r["displayname"]
+                repositories[k] = v 
+            mongo_client.close()
+            return repositories
+        except Exception as err:
+            current_app.logger.info("Error: unable to load repository table from mongodb, {}", err)
+            return repositories
 
     def revert_task(self, job_ticket_id, task_name):
         return True
