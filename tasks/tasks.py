f46f415867e79cc939aed6f954d390486e86d510
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-04-14 16:03:46 -0400

    disable acks late

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -15,7 +15,8 @@ http_client = requests.Session()
 http_client.mount("https://", adapter)
 http_client.mount("http://", adapter)
 
-@celery.task(ignore_result=False, acks_late=True)
+#@celery.task(ignore_result=False, acks_late=True)
+@celery.task(ignore_result=False)
 def do_task(message):
     url = "https://localhost:8081/jstor_transformer/do_task"
     response = http_client.post(url, json = message, verify=False)
